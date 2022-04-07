ecc5a97f7a83c3a9d9a4f6b3c4e67d56da0fe2a2
aasimgcg <shaikh.aasim@gcloudg.com>
2022-04-07 13:55:21 +0530

    studentAPI

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-server.listen(3000, console.log("app is running"));
+server.listen(3001, console.log("app is running"));
